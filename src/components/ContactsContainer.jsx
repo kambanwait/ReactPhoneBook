7a696067e552ae5c42f78439c2df18ecf0415a8f
KamBanwait <kam@scriptedpixels.co.uk>
2019-02-16 13:28:00 +0000

    sort list by name - ascending/descending

--- src/components/ContactsContainer.jsx
+++ src/components/ContactsContainer.jsx
@@ -5,11 +5,32 @@ import ContactsList from './ContactsList'
 
 export default (props) => {
 
+  // sort the list ascending/descending depending on state value
+  const sortByName = (a, b) => {
+    if (props.sortContactValue === 'ascending') {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+    } else {
+      if (a.name > b.name) {
+        return -1;
+      }
+      if (a.name < b.name) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
   const contactList = props.contacts
     .filter(contact => { // contact = individual array item
       // check each contact.name value for filtered text and only show the "true" result
       return contact.name.toLowerCase().indexOf(props.filterText.toLowerCase()) >= 0;
     })
+    .sort(sortByName)
     .map((contact, index) => {
       return (
         <li key={index}>
@@ -25,7 +46,11 @@ export default (props) => {
 
   return (
     <React.Fragment>
-      <SortSearchContacts filterUpdate={props.filterUpdate} />
+      <SortSearchContacts
+        filterUpdate={props.filterUpdate}
+        sortContactValue={props.sortContactValue}
+        sortContacts={props.sortContacts}
+      />
       <ContactsList contactsSorted={contacts} />
     </React.Fragment>
   );
